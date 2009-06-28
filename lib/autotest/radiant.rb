252410bf903d58e90546bc9b9c9ada9b87e90752
Josh French <josh@vitamin-j.com>
2009-06-28 17:12:42 -0400

    Move non-notification functions into autotest/radiant
    Exposing total, failed, and pending spec counts as they might be
    useful to someone else.

--- lib/autotest/radiant.rb
+++ lib/autotest/radiant.rb
@@ -50,8 +50,33 @@ Autotest.add_hook :initialize do |at|
 end
 
 class Autotest::Radiant < Autotest::Rspec
+  attr_reader :count_re
+  attr_writer :examples_total, :examples_failed, :examples_pending
+
   def initialize
     super
     @count_re = /(\d+) examples?, (\d+) failures?(?:, (\d+) pending)?/
   end
+
+  def success_count
+    examples_total - examples_pending - examples_failed
+  end
+
+  %w(examples_total examples_failed examples_pending).each do |attr|
+    class_eval <<-EOS, __FILE__, __LINE__
+      def #{attr}             # def examples_total
+        @#{attr} ||= 0        #   @examples_total ||= 0
+      end                     # end
+    EOS
+  end
+
+end
+
+Autotest.add_hook :ran_command do |at|
+  at.results.grep at.count_re
+  at.examples_total, at.examples_failed, at.examples_pending = $1.to_i, $2.to_i, $3.to_i
+end
+
+def spec_string(n=1)
+  1 == n ? "#{n} spec" : "#{n} specs"
 end
--- lib/autotest/radiant/growl.rb
+++ lib/autotest/radiant/growl.rb
@@ -1,12 +1,6 @@
 require 'autotest/rspec'
 
 class Autotest::Radiant < Autotest::Rspec
-  attr_reader :count_re
-  attr_writer :examples_total, :examples_failed, :examples_pending
-
-  def success_count
-    examples_total - examples_pending - examples_failed
-  end
 
   def growl title, msg, img, pri=0
     system "growlnotify -n autotest --image #{img} -p #{pri} -m #{msg.inspect} #{title}"
@@ -22,22 +16,6 @@ class Autotest::Radiant < Autotest::Rspec
     File.exists?(local) ? local : File.expand_path(File.join(__FILE__, %w(.. .. .. fail.png)))
   end
 
-  %w(examples_total examples_failed examples_pending).each do |attr|
-    class_eval <<-EOS, __FILE__, __LINE__
-      def #{attr}             # def examples_total
-        @#{attr} ||= 0        #   @examples_total ||= 0
-      end                     # end
-    EOS
-  end
-end
-
-def spec_string(n=1)
-  1 == n ? "#{n} spec" : "#{n} specs"
-end
-
-Autotest.add_hook :ran_command do |at|
-  at.results.grep at.count_re
-  at.examples_total, at.examples_failed, at.examples_pending = $1.to_i, $2.to_i, $3.to_i
 end
 
 Autotest.add_hook :red do |at|
